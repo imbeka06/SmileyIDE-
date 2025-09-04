7b6fb58a019c40648157c05f67ecade13a737b7d
IMBEKA <imbeka06@gmail.com>
2025-09-04 06:43:33 +0300

    added web preview functionality

--- js/languages/html_css_js.js
+++ js/languages/html_css_js.js
@@ -1,4 +1,14 @@
-function updatePreview(code) {
-    const preview = document.getElementById('preview');
-    preview.innerHTML = code;
+// Web preview functionality
+
+function updateWebPreview() {
+    const code = getCode();
+    const preview = document.getElementById('webPreview');
+    
+    if (preview) {
+        const blob = new Blob([code], { type: 'text/html' });
+        preview.src = URL.createObjectURL(blob);
+    }
 }
+
+// Make function available globally
+window.updateWebPreview = updateWebPreview;
